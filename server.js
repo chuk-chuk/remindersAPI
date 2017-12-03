edb907fa203fe85a4a2f04dda2f9b65b89c58d90
Yulia Litvinchuk <yulialitvinchuk@PEG-MAC-009751.local>
2017-12-03 18:28:14 +0000

    added POST and GETS routes for the application

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ var port = process.env.PORT || 8080;        // set our port
 var router = express.Router();              // get an instance of the express Router
 
 router.use(function(req, res, next) {
-  console.log('something happened');
+  console.log('something is happening');
   next();
 });
 
@@ -33,6 +33,35 @@ router.get('/', function(req, res) {
 });
 
 // more routes for our API will happen here
+router.route('/reminders')
+
+// create a reminder (accessed at POST http://localhost:8080/api/reminders)
+    .post(function(req, res) {
+
+        var reminder = new Reminder();      // create a new instance of the Reminder model
+        reminder.created_at = req.body.created_at;
+        reminder.text = req.body.text;  // set the reminder dates (comes from the request)
+        reminder.expired_by = req.body.expired_by;
+
+        // save the reminder and check for errors
+        reminder.save(function(err) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Reminder created!' });
+        });
+
+    });
+
+// get all the reminders (accessed at GET http://localhost:8080/api/reminders)
+      .get(function(req, res) {
+          Reminder.find(function(err, reminders) {
+              if (err)
+                  res.send(err);
+
+              res.json(reminders);
+          });
+      });
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
