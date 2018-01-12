680899c062d39e6ab3a2a24ce1fa96a30a0e978b
Yulia Litvinchuk <yulia.v.litvin@gmail.com>
2018-01-12 17:22:35 +0000

    add edit route

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+const cors = require('cors');
 // var Reminder = require('./models/reminders');
 var mongojs = require('mongojs');
 var db = mongojs('mongodb://localhost:27017/Reminders'); //connect to DB
+var ObjectID = require('mongodb').ObjectID;
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;
@@ -70,6 +73,32 @@ router.post('/reminder-new', function(req, res) {
     };
   })
 });
+
+//edit reminder
+router.put('/reminders/:reminderId', function(req, res) {
+  var reminderId = req.params._id;
+  console.log(req.body)
+  db.collection('reminders-collection').update(
+    {_id :  ObjectID(req.body._id) },
+    { $set: { 'text': req.body.text, 'expired_by': req.body.expired_by}
+  }, function(err){
+    if(err) {res.send(err)} else {
+      res.json({message: 'reminder edited!'});
+    };
+  })
+});
+
+//delete reminder to check
+router.delete('/reminders/:reminderId', function(req, res) {
+  var reminderId = req.params._id;
+  db.collection('reminders-collection').remove(reminderId, (err, result) => {
+    if (err) {return console.log(err)} else {
+      console.log("hello");
+        res.json({message: 'reminder deleted'});
+    };
+  });
+});
+
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
 app.use('/api', router);
