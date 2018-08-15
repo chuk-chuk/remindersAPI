3f804938f426b66aafe67816ae36b64cfad44bad
Yulia Litvinchuk <yulia.v.litvin@gmail.com>
2018-08-15 16:40:15 +0100

    Separate out all db queries into a separate file

--- service.js
+++ service.js
@@ -1,6 +1,6 @@
 
 const mongojs = require('mongojs');
-const db = mongojs('mongodb://localhost:27017/Reminders'); //connect to DB
+const db = mongojs('mongodb://localhost:27017/Reminders');
 const ObjectID = require('mongodb').ObjectID;
 const collection = db.collection('reminders-collection');
 
@@ -9,24 +9,34 @@ module.exports.getAllReminders = (cb) => {
 }
 
 module.exports.getReminderByDate = (createdDate, cb) => {
-    collection.find({ created_at: createdDate }, cb)
+    collection.find({ created_at: createdDate }, cb) 
 }
 
 module.exports.getReminderByContent = (content, cb) => {
     collection.find({ text: content }, cb)
 }
-module.exports.deleteReminderById = (id, cb) => {
-    collection.remove({_id: ObjectID(id)}, cb)
-}
 
-module.exports.updateReminder = (id, newValue, cb) => {
+module.exports.updateReminder = (id, newValueText, newValueExpiry, cb) => {
+    const update = {};
+
+    if(typeof newValueText != "undefined" && newValueText) {
+        update.text = newValueText
+    }
+    if(typeof newValueExpiry != "undefined" && newValueExpiry) {
+        update.expired_by = newValueExpiry
+    }
+
     collection.findAndModify({
         query: {_id: ObjectID(id)},
         update: { 
-          $set: {text: newValue}
+            $set: update
         },
         upsert: false,
         returnOriginal: false,
         new: true,
-      }, cb)
+    }, cb)
+}
+
+module.exports.deleteReminderById = (id, cb) => {
+    collection.remove({_id: ObjectID(id)}, cb)
 }
